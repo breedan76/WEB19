4ec076ca743a255ed89682af402fa7419599a122
breedan76 <brice@savbox.fr>
2020-09-03 16:20:27 +0200

    Nouvelle ligne

--- brice.php
+++ brice.php
@@ -3,3 +3,4 @@
 echo '<h1>ceci est le fichier de  brice la vieille bique</h1>';
 echo '<h1>ceci est le fichier de  brice la vieille bique </h1>';
 echo '<h1>grosse folle </h1>';
+echo '<h1>Voici mon code perso facturé 22220202020202€ </h1>';
